5c80a2d659c2676199ab2dc16bc23ecd5ecbcff5
nerdyslacker <karyan40024@gmail.com>
2025-03-08 16:51:01 +0400

    update node version and fix netlify function issues

--- netlify/functions/submission-created.js
+++ netlify/functions/submission-created.js
@@ -1,6 +1,6 @@
-import { createTransport } from 'nodemailer'
+const nodemailer = require('nodemailer')
 
-export async function handler(event) {
+exports.handler = async function handler(event) {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed', headers: { Allow: 'POST' } }
   }
@@ -11,7 +11,7 @@ export async function handler(event) {
   }
 
   try {
-    const transporter = createTransport({
+    const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
       auth: {
@@ -26,29 +26,15 @@ export async function handler(event) {
       from: `${process.env.SMTP_SENDER_NAME} <${process.env.SMTP_SENDER_EMAIL}>`,
       to: data.email,
       subject: 'Application Confirmation',
-      text: `Dear ${data.name},\n\n
-
-        Thank you for applying for the ${data['form-name']} position at Armat Analytics. We have received your application and will review it shortly. If your qualifications match our needs, we will contact you for the next steps.\n\n
-        
-        Best regards,\n
-        Armat Analytics`,
+      text: `Dear ${data.name},\n\nThank you for applying for the ${data['form-name']} position at Armat Analytics. We have received your application and will review it shortly. If your qualifications match our needs, we will contact you for the next steps.\n\nBest regards,\nArmat Analytics`,
     }
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(`Failed to send email. Error: ${error.message}`)
-        return {
-          statusCode: 500,
-          body: JSON.stringify({ error: err.message }),
-        }
-      } else {
-        console.log('Email sent successfully:', info)
-        return {
-          statusCode: 200,
-          body: JSON.stringify({ message: 'Email sent successfully!' }),
-        }
-      }
-    })
+    await transporter.sendMail(mailOptions)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Emails sent successfully!' }),
+    }
   } catch (err) {
     return {
       statusCode: 500,
